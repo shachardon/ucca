46b28a19d762b4c99fe1932c25e8e571db750731
Daniel Hershcovich <danielh@cs.huji.ac.il>
2015-01-06 19:27:06 +0200

    Script to print trees in PTB format (s-expressions)

---
+++ scripts/trees_to_ptb.py
@@ -0,0 +1,12 @@
+from tree import *
+
+os.chdir("..")
+try:
+    trees = load_trees()
+except FileNotFoundError:
+    trees = build_trees()
+
+fname = "trees/data.txt"
+with open(fname, "w") as f:
+    f.writelines([str(tree) + "\n" for tree in trees])
+print("Wrote '%s'" % fname)
