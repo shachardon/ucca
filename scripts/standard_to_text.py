d6269e962c5b1413f9b5bab318796c20730374d2
Daniel Hershcovich <danielh@cs.huji.ac.il>
2015-01-20 18:38:39 +0200

    Script to extract just the text from a UCCA passage

---
+++ scripts/standard_to_text.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+
+desc = """Parses an XML in UCCA standard format, and writes just the text.
+"""
+
+import argparse
+import sys
+from xml.etree.ElementTree import ElementTree
+
+import ucca.convert
+
+
+def file2passage(filename):
+    "Opens a file and returns its parsed Passage object"
+    with open(filename) as f:
+        etree = ElementTree().parse(f)
+    return ucca.convert.from_standard(etree)
+
+
+def main():
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('filenames', nargs='+', help="XML file names to convert")
+    parser.add_argument('-o', '--outfile', help="output file for text")
+    args = parser.parse_args()
+
+    handle = open(args.outfile, 'w') if args.outfile else sys.stdout
+    for filename in args.filenames:
+        sys.stderr.write("Reading passage '%s'...\n" % filename)
+        passage = file2passage(filename)
+        output = ucca.convert.to_text(passage)
+        handle.write("\n".join(output))
+        handle.write("\n")
+    if handle is not sys.stdout:
+        handle.close()
+
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
