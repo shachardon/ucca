80d16dde050cd942023d5c9014aae45b373c1c57
Daniel Hershcovich <danielh@cs.huji.ac.il>
2015-05-14 12:00:22 +0300

    Move evaluate.py to scripts

--- toy/test.sh
+++ toy/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python3 ../scripts/standard_to_conll.py toy.xml || exit 1
+python3 ../scripts/standard_to_conll.py ../doc/toy.xml || exit 1
 python3 ../scripts/conll_to_standard.py ucca_passage504.conll || exit 1
-python3 ../private/evaluate.py -fr toy.xml -g ucca_passage504.xml || exit 1
+python3 ../scripts/evaluate.py -fr ../doc/toy.xml -g ucca_passage504.xml || exit 1
