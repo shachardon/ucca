d43ba6e7a27283a84b62363d65d466a38ff52dfc
Daniel Hershcovich <danielh@cs.huji.ac.il>
2015-08-31 15:57:41 +0300

    Support unary parents in transition-based parents
    Still fails to reproduce original passage. Need to support multiple children.

--- parser/oracle.py
+++ parser/oracle.py
@@ -5,19 +5,49 @@ Implements the arc-eager algorithm.
 """
 
 
+class Oracle:
+    def __init__(self, passage):
+        self.edges_done = set()
+        self.passage = passage
+
+    def filter_edges_done(self, edges):
+        return [e for e in edges if e not in self.edges_done]
+
+    def get_action(self, config):
+        if config.stack and config.buffer:
+            s = self.passage.by_id(config.stack[-1].node_id)
+            b = self.passage.by_id(config.buffer[0].node_id)
+            for edge in self.filter_edges_done(s.incoming):
+                if len(edge.parent.outgoing) == 1:
+                    self.edges_done.add(edge)
+                    return "UNARY-" + edge.tag, edge.parent.ID
+            for edge in self.filter_edges_done(b.outgoing):  # FIXME not possible since only terminals are on buffer
+                if edge.child.ID == s.ID:
+                    self.edges_done.add(edge)
+                    return "LEFT-ARC-" + edge.tag, edge.parent.ID
+            for edge in self.filter_edges_done(s.outgoing):
+                if edge.child.ID == b.ID:
+                    self.edges_done.add(edge)
+                    return "RIGHT-ARC-" + edge.tag, edge.parent.ID
+            if not self.filter_edges_done(s.outgoing + s.incoming):
+                return "REDUCE", None
+        return "SHIFT", None
+
+"""
 def get_action(passage, config):
     if config.stack and config.buffer:
         s = config.stack[-1]
         b = config.buffer[0]
-        s_node = passage.by_id(s)
-        b_node = passage.by_id(b)
-        for edge in b_node.outgoing:
-            if edge.child.ID == s:
-                return "LEFT-ARC-" + edge.tag
-        for edge in s_node.outgoing:
-            if edge.child.ID == b:
-                return "RIGHT-ARC-" + edge.tag
-        for edge in b_node.outgoing + b_node.incoming:
-            if edge.child.ID < s:
+        if len(s.incoming) == 1:
+            return "UNARY-" + s.incoming.itervalues().next()
+        for child_index, edge in b.outgoing.items():
+            if child_index == s.index:
+                return "LEFT-ARC-" + edge
+        for child_index, edge in s.outgoing.items():
+            if child_index == b.index:
+                return "RIGHT-ARC-" + edge
+        for child_index, edge in b.outgoing.items():# + b.incoming.items():
+            if child_index < s.index:
                 return "REDUCE"
     return "SHIFT"
+"""
--- parser/parser.py
+++ parser/parser.py
@@ -14,69 +14,120 @@ from ucca import layer0, layer1
 from ucca.core import Passage
 from ucca.convert import from_text
 from scripts.util import file2passage, passage2file
-import oracle
+from oracle import Oracle
 
 
 class Configuration:
+
+    class Node:
+        def __init__(self, index, text=None, node_id=None):
+            self.index = index
+            self.text = text
+            self.node_id = node_id
+            self.outgoing = {}
+            self.incoming = {}
+            self.node = None
+
+        def __repr__(self):
+            return self.text or self.node_id or "Node(%d)" % self.index
+
+        def add_edge(self, child, tag):
+            self.outgoing[child.index] = tag
+            child.incoming[self.index] = tag
+            print("    %s->%s" % (tag, child))
+
     def __init__(self, passage, passage_id):
         if isinstance(passage, Passage):
-            terminals = [x.ID for x in passage.layer(layer0.LAYER_ID).all]
-            tokens = [[x.text for x in xs]
-                      for _, xs in groupby(passage.layer(layer0.LAYER_ID).all,
-                                           key=attrgetter('paragraph'))]
+            self.nodes = [Configuration.Node(i, text=x.text, node_id=x.ID) for i, x in
+                          enumerate(passage.layer(layer0.LAYER_ID).all)]
+            self.tokens = [[x.text for x in xs]
+                           for _, xs in groupby(passage.layer(layer0.LAYER_ID).all,
+                                                key=attrgetter('paragraph'))]
         else:
-            tokens = [token for paragraph in passage for token in paragraph]
-            terminals = ["0.%d" % (i+1) for i in enumerate(tokens)]
-        paragraphs = [' '.join(paragraph) for paragraph in tokens]
-        self.stack = []
-        self.buffer = deque(terminals)
-        self.passage = from_text(paragraphs, passage_id)
+            self.tokens = [token for paragraph in passage for token in paragraph]
+            self.nodes = [Configuration.Node(i, text=x) for i, x in enumerate(self.tokens)]
+        self.buffer = deque(self.nodes)
+        self.stack = [self.add_node("1.1")]
+        self.passage_id = passage_id
 
     def is_terminal(self):
         return not self.buffer
 
-    def apply_action(self, action):
-        m = re.match("LEFT-ARC-(.*)", action)
+    def apply_action(self, action, node_id=None):
+        m = re.match("(.*)-(.*)", action)
         if m:
-            return self.apply_left_arc(m.group(0))
-        m = re.match("RIGHT-ARC-(.*)", action)
-        if m:
-            return self.apply_right_arc(m.group(0))
-        if action == "REDUCE":
-            return self.apply_reduce()
-        if action == "SHIFT":
-            return self.apply_shift()
-        raise Exception("Invalid action: " + action)
+            action_type, tag = m.groups()
+            if action_type == "UNARY":
+                parent = self.add_node(node_id)
+                parent.add_edge(self.stack.pop(), tag)
+                self.stack.append(parent)
+            elif action_type == "LEFT-ARC":
+                self.buffer[0].add_edge(self.stack.pop(), tag)
+            elif action_type == "RIGHT-ARC":
+                b = self.buffer[0]
+                self.stack[-1].add_edge(b, tag)
+                self.stack.append(b)
+            else:
+                raise Exception("Invalid action: " + action_type)
+        elif action == "REDUCE":
+            self.stack.pop()
+        elif action == "SHIFT":
+            self.stack.append(self.buffer.popleft())
+        else:
+            raise Exception("Invalid action: " + action)
 
-    def apply_left_arc(self, tag):
-        return self.add_edge(self.buffer[0], self.stack.pop(), tag)
+    def add_node(self, node_id=None):
+        n = Configuration.Node(len(self.nodes), node_id=node_id)
+        self.nodes.append(n)
+        print("    %s" % n)
+        return n
 
-    def apply_right_arc(self, tag):
-        b = self.buffer[0]
-        edge = self.add_edge(self.stack[-1], b, tag)
-        self.stack.append(b)
-        return edge
+    @property
+    def passage(self):
+        paragraphs = [' '.join(paragraph) for paragraph in self.tokens]
+        passage = from_text(paragraphs, self.passage_id)
+        terminals = passage.layer(layer0.LAYER_ID).all
+        l1 = layer1.Layer1(passage)
+        linkage = []
+        for n in reversed(self.nodes):
+            if all(t[0] == "L" for t in n.outgoing.values()):
+                linkage.append(n)
+                continue
+            for child_index, tag in n.outgoing.items():
+                child = self.nodes[child_index]
+                child.node = self.add_layer1_node(child, child_index, l1, n, tag, terminals)
+        for n in linkage:
+            link_relation = (self.nodes[i].node for i, t in n.outgoing.items()
+                             if t == layer1.EdgeTags.LinkRelation)[0]
+            link_args = (self.nodes[i].node for i, t in n.outgoing.items()
+                         if t == layer1.EdgeTags.LinkArgument)
+            n.node = l1.add_linkage(link_relation, link_args)
+        return passage
 
-    def apply_reduce(self):
-        return self.stack.pop()
-
-    def apply_shift(self):
-        return self.stack.append(self.buffer.popleft())
-
-    def add_edge(self, i, j, tag):
-        return self.passage.by_id(i).add(tag, self.passage.by_id(j))
+    def add_layer1_node(self, child, child_index, l1, n, tag, terminals):
+        if child.text:
+            return n.node.add(layer1.EdgeTags.Terminal, terminals[child_index]).child
+        if len(child.outgoing) == 1:
+            grandchild_index = next(iter(child.outgoing))
+            grandchild = self.nodes[grandchild_index]
+            if grandchild.text:
+                t = terminals[grandchild_index]
+                if layer0.is_punct(t):
+                    return l1.add_punct(n.node, t)
+        return l1.add_fnode(n.node, tag)
 
 
 class Parser:
     def __init__(self):
         self.config = None
-        self.actions = [action + relation for action in ("LEFT-ARC-", "RIGHT-ARC-")
-                        for relation in layer1.EdgeTags.__dict__.values()
-                        if isinstance(relation, str)] + ["REDUCE", "SHIFT"]
+        self.actions = [action + relation for action in ("UNARY-", "LEFT-ARC-", "RIGHT-ARC-")
+                        for name, relation in layer1.EdgeTags.__dict__.items()
+                        if isinstance(relation, str) and not name.startswith('__')] +\
+                       ["REDUCE", "SHIFT"]
         self.actions_reverse = {action: i for i, action in enumerate(self.actions)}
         self.features = [lambda config: len(config.stack),
                          lambda config: len(config.buffer)]
-        self.weights = np.zeros((len(self.actions), len(self.features)))
+        self.weights = 0.01 * np.random.randn(len(self.actions), len(self.features))
 
     def train(self, passages, iterations=1):
         print("%d training passages" % len(passages))
@@ -86,16 +137,23 @@ class Parser:
             actions = 0
             for passage in passages:
                 self.config = Configuration(passage, passage.ID)
+                oracle = Oracle(passage)
                 while not self.config.is_terminal():
                     pred_action = self.predict_action()
-                    true_action = oracle.get_action(passage, self.config)
+                    true_action, node_id = oracle.get_action(self.config)
                     if pred_action != true_action:
                         self.update(pred_action, true_action)
                     else:
                         correct += 1
-                    self.config.apply_action(true_action)
+                    # print("  pred: %-15s true: %-15s stack: %-20s buffer: %-70s" %
+                    print("  %-15s stack: %-20s buffer: %-70s" %
+                          (true_action, self.config.stack, list(self.config.buffer)))
+                    self.config.apply_action(true_action, node_id)
                     actions += 1
-                    print("  pred: %-20s true: %s" % (pred_action, true_action))
+                outfile = "%s/%s%s.xml" % (args.outdir, args.prefix, passage.ID)
+                sys.stderr.write("Writing passage '%s'...\n" % outfile)
+                passage2file(self.config.passage, outfile)
+                assert passage.equals(self.config.passage), "Oracle failed to produce true passage"
             print("Accuracy: %.3f (%d/%d)\n" % (correct/actions, correct, actions)
                   if actions else "No actions done")
 
