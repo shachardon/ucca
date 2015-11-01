05fb1395afc379ca0fa34644ff141816388690e6
Daniel Hershcovich <danielh@cs.huji.ac.il>
2015-11-01 14:52:15 +0200

    setup.py instead of setting PYTHONPATH

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='UCCA',
+      version='1.0',
+      description='Universal Conceptual Cognitive Annotation',
+      author='Daniel Hershcovich',
+      author_email='danielh@cs.huji.ac.il',
+      url='http://www.cs.huji.ac.il/~oabend/ucca.html',
+      packages=['ucca', 'scripts', 'parser'],
+      )
