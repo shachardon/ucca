719123a5e5226f0a953ac57d5bb07e232b3f63cd
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2015-11-05 19:38:43 +0200

    Get state as shared parameter for all features

--- parser/features.py
+++ parser/features.py
@@ -1,13 +1,11 @@
 class FeatureExtractor(object):
-    def __init__(self):
-        self.features = {
-            "#s": lambda state: len(state.stack),
-            "#b": lambda state: len(state.buffer)
-        }
-
-    def extract_features(self, state):
+    @staticmethod
+    def extract_features(state):
         """
         Calculate feature values according to current state
         :param state: current state of the parser
         """
-        return {feature: f(state) for feature, f in self.features.items()}
+        return {
+            "#s": len(state.stack),
+            "#b": len(state.buffer),
+        }
