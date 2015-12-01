d31ef895ca148db91969acb1a98618be4bda3c84
Daniel Hershcovich <danielh@cs.huji.ac.il>
2015-12-01 12:15:31 +0200

    Go back to representing weights as dictionary
    This reverts the change from 6b20b699ff070493339ee310205a2df0188fa245.
    Apparently my implementation was very inefficient, since training took 4 times as long and would often get killed from memory overuse.
    Until a more efficient implementation, using the simple dictionary approach.

--- parser/averaged_perceptron.py
+++ parser/averaged_perceptron.py
@@ -1,5 +1,5 @@
-import os
 import shelve
+from collections import defaultdict
 
 import numpy as np
 
@@ -11,28 +11,10 @@ class AveragedPerceptron(object):
 
     def __init__(self, num_actions):
         self.num_actions = num_actions
-        self.feature_dictionary = {}
-        self.weights = []
-        self._last_update = []
-        self._totals = []
+        self.weights = defaultdict(lambda: 0.01 * np.random.randn(self.num_actions))
+        self._last_update = defaultdict(lambda: np.zeros(self.num_actions, dtype=int))
+        self._totals = defaultdict(lambda: np.zeros(self.num_actions, dtype=float))
         self._update_index = 0
-
-    def _get_feature_index(self, feature, frozen=False):
-        index = self.feature_dictionary.get(feature)
-        if index is None and not frozen:
-            self.weights.append(0.01 * np.random.randn(self.num_actions))
-            self._last_update.append(np.zeros(self.num_actions, dtype=int))
-            self._totals.append(np.zeros(self.num_actions, dtype=float))
-            index = len(self.feature_dictionary)
-            self.feature_dictionary[feature] = index
-        return index
-
-    def _get_feature_indices_and_values(self, features, frozen=False):
-        res = [(self._get_feature_index(feature, frozen), value)
-               for feature, value in features.items() if value]
-        if frozen:
-            res = [(index, value) for index, value in res if index is not None]
-        return zip(*res)
 
     def score(self, features):
         """
@@ -40,14 +22,15 @@ class AveragedPerceptron(object):
         :param features: extracted feature values
         :return: array of score for each action
         """
-        is_array = isinstance(self.weights, np.ndarray)
-        indices, values = self._get_feature_indices_and_values(features, frozen=is_array)
-        if is_array:
-            return self.weights[list(indices)].T.dot(values)
-        return np.array([sum(self.weights[i][j] * value
-                             for i, value in zip(indices, values))
-                         for j in range(self.num_actions)])
-        # TODO check speed of keeping self.weights as an array and using vstack/append
+        scores = np.zeros(self.num_actions)
+        for feature, value in features.items():
+            if not value:
+                continue
+            weights = self.weights.get(feature)
+            if weights is None:
+                continue
+            scores += value * weights
+        return scores
 
     def update(self, features, pred_action, true_action, learning_rate=1):
         """
@@ -64,29 +47,29 @@ class AveragedPerceptron(object):
             self._last_update[f][a] = self._update_index
 
         self._update_index += 1
-        indices, values = self._get_feature_indices_and_values(features)
-        for index, value in zip(indices, values):
-            update_feature(index, true_action.id, learning_rate * value)
-            update_feature(index, pred_action.id, -learning_rate * value)
+        for feature, value in features.items():
+            if not value:
+                continue
+            update_feature(feature, true_action.id, learning_rate * value)
+            update_feature(feature, pred_action.id, -learning_rate * value)
 
     def average_weights(self):
         """
         Average all weights over all updates, as a form of regularization
-        Locks the weights matrix to a numpy array
         """
-        n = np.array(self._update_index) - self._last_update
-        self._totals += + n * self.weights
-        self.weights = self._totals / self._update_index
+        for feature in self.weights:
+            n = self._update_index - self._last_update[feature]
+            self._totals[feature] += n * self.weights[feature]
+            self.weights[feature] = self._totals[feature] / self._update_index
+        self.weights = dict(self.weights)  # "Lock" set of features; also allow pickle
 
     def save(self, filename):
         with shelve.open(filename) as db:
-            db["feature_dictionary"] = self.feature_dictionary
             db["weights"] = self.weights
 
     def load(self, filename):
         try:
             with shelve.open(filename, flag="r") as db:
                 self.weights = db["weights"]
-                self.feature_dictionary = db["feature_dictionary"]
         except:
             raise IOError("Model file not found: " + filename)
