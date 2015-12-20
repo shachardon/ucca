1178edf13704313f146b34f31fd8efffddd2c39b
Daniel Hershcovich <danielh@cs.huji.ac.il>
2015-12-20 15:40:14 +0200

    Add option to annotate original node IDs when converting to sentences

--- scripts/standard_to_sentences.py
+++ scripts/standard_to_sentences.py
@@ -15,11 +15,12 @@ def main():
     parser.add_argument('filenames', nargs='+', help="passage file names to convert")
     parser.add_argument('-o', '--outdir', default='.', help="output directory")
     parser.add_argument('-p', '--prefix', default='ucca_passage', help="output filename prefix")
+    parser.add_argument('-r', '--remarks', action='store_true', help="annotate original IDs")
     args = parser.parse_args()
 
     for filename in args.filenames:
         passage = file2passage(filename)
-        sentences = ucca.convert.split2sentences(passage)
+        sentences = ucca.convert.split2sentences(passage, remarks=args.remarks)
         for i, sentence in enumerate(sentences):
             outfile = "%s/%s%s_%d.xml" % (args.outdir, args.prefix, sentence.ID, i)
             sys.stderr.write("Writing passage file for sentence '%s'...\n" % outfile)
