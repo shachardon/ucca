bb116d5ad4e9fe7a1b056bcddd9819005d74bbfa
Daniel Hershcovich <danielh@cs.huji.ac.il>
2016-02-11 13:01:09 +0200

    Test for oracle actions on standard test passage

--- parser/tests/test_parser.py
+++ parser/tests/test_parser.py
@@ -2,6 +2,24 @@
 
 import unittest
 
+from ucca import convert
+from ucca.tests.test_ucca import TestUtil
+from state import State
+from oracle import Oracle
+
 
 class ParserTests(unittest.TestCase):
-    pass
+    def test_oracle(self):
+        passage = convert.from_standard(TestUtil.load_xml('test_files/standard3.xml'))
+        oracle = Oracle(passage)
+        state = State(passage, passage.ID)
+        actions_taken = []
+        while True:
+            actions = oracle.get_actions(state)
+            action = next(iter(actions))
+            state.transition(action)
+            actions_taken.append("%s\n" % action)
+            if state.finished:
+                break
+        with open('test_files/standard3.oracle_actions.txt') as f:
+            self.assertSequenceEqual(actions_taken, f.readlines())
