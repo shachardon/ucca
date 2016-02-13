497259feae57af7d4ff7cb4b754956f0e25d8961
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2016-02-13 11:04:32 +0200

    Fix conversion test scripts to specify prefix when necessary

--- test_files/test_convert_all.sh
+++ test_files/test_convert_all.sh
@@ -5,9 +5,9 @@ cd $(dirname $0)
 mkdir -p converted
 for format in conll sdp export txt; do
     if [ $# -lt 1 -o "$format" = "$1" ]; then
-        for passage in ../pickle/*.pickle; do
+        for passage in ../pickle/ucca_passage*.pickle; do
             base=$(basename $passage .pickle)
-            python3 ../scripts/convert_from_standard.py $passage -f $format -o converted || exit 1
+            python3 ../scripts/convert_from_standard.py $passage -f $format -o converted -p "ucca_passage" || exit 1
             python3 ../scripts/convert_to_standard.py converted/$base.$format -f $format -o converted -b -p "ucca_passage" || exit 1
             python3 ../scripts/evaluate_standard.py -f converted/$base.pickle $passage || exit 1
         done
