5926b7f071f3e9ff0025eeccf0dd62ef67caae82
Daniel Hershcovich <danielh@cs.huji.ac.il>
2016-03-01 13:59:30 +0200

    Use Travis build matrix to also run the parser and converters on the whole corpus

---
+++ ci/test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+case "$TEST_SUITE" in
+unit)
+    # unit tests
+    python -m unittest discover -v
+    # basic conversion test
+    ci/test_convert_toy.sh
+    # basic parser tests
+    python parsing/parse.py -I 10 -t doc/toy.xml -d doc/toy.xml -vm model_toy
+    python parsing/parse.py doc/toy.xml -evm model_toy
+    python parsing/parse.py -I 10 -t doc/toy.xml -d doc/toy.xml -svm model_toy_sentences
+    python parsing/parse.py doc/toy.xml -esvm model_toy_sentences
+    python parsing/parse.py -I 10 -t doc/toy.xml -d doc/toy.xml -avm model_toy_paragraphs
+    python parsing/parse.py doc/toy.xml -esvm model_toy_paragraphs
+    ;;
+parse_all)
+    python parsing/parse.py -Webs pickle/test -t pickle/train -d pickle/dev -m model
+    ;;
+convert_all)
+    ci/test_convert_all.sh
+    ci/test_convert_all_sentences.sh
+    ;;
+esac
---
+++ ci/test_convert_all_sentences.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -x
+
+cd $(dirname $0)
+mkdir -p converted-sentences
+for format in conll sdp export "export --tree" txt; do
+    if [ $# -lt 1 -o "$format" = "$1" ]; then
+        for passage in ../pickle/sentences/ucca_passage*.pickle; do
+            base=$(basename $passage .pickle)
+            python3 ../scripts/convert_from_standard.py $passage -f $format -o converted-sentences -p "ucca_passage" || exit 1
+            python3 ../scripts/convert_to_standard.py converted-sentences/$base.$format -f $format -o converted-sentences -b -p "ucca_passage" || exit 1
+            python3 ../scripts/evaluate_standard.py -f converted-sentences/$base.pickle $passage || exit 1
+        done | tee "$format.sentences.log"
+    fi
+done
