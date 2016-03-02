a58542331d29587b4240b0d8393e5fb4814d753a
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2016-03-02 13:17:52 +0200

    Output only F1 from conversion to limit log size

--- ci/test_convert_all_sentences.sh
+++ ci/test_convert_all_sentences.sh
@@ -8,7 +8,7 @@ for format in conll sdp export "export --tree" txt; do
             base=$(basename $passage .pickle)
             python3 ../scripts/convert_from_standard.py $passage -f $format -o converted-sentences -p "ucca_passage" || exit 1
             python3 ../scripts/convert_to_standard.py converted-sentences/$base.$format -f $format -o converted-sentences -b -p "ucca_passage" || exit 1
-            python3 ../scripts/evaluate_standard.py -f converted-sentences/$base.pickle $passage || exit 1
+            python3 ../scripts/evaluate_standard.py -f converted-sentences/$base.pickle $passage | grep F1 || exit 1
         done | tee "$format.sentences.log"
     fi
 done
