14d4cf42b6014e01e1ab5a96e6d9b8a0a4c8de26
Daniel Hershcovich <danielh@cs.huji.ac.il>
2016-04-10 18:21:33 +0300

    Add dense perceptron class
    Not used yet, but will allow using embedding features efficiently.
    Also remove the "true labels" feature from the perceptron, as it was not used, because all of the original labels have always been in use (except for FINISH for some reason, but this should be investigated). It was originally added because the actions for all edge tags were added at construction, but now new actions are added on-the-fly and no edge actions exist at initialization.
     Renamed the original perceptron to sparse perceptron (which uses dictionaries from feature to weights vector).
     Added a Classifier super-class to handle common code. NeuralNetwork will also inherit from it.

--- scripts/model_to_tsv.py
+++ scripts/model_to_tsv.py
@@ -4,7 +4,9 @@ import argparse
 import os
 import sys
 
-from parsing import util, perceptron
+from parsing import util
+from parsing.dense_perceptron import DensePerceptron
+from parsing.sparse_perceptron import SparsePerceptron
 
 desc = """Reads a model file in pickle format and writes as TSV
 """
@@ -12,11 +14,12 @@ desc = """Reads a model file in pickle format and writes as TSV
 
 def main():
     argparser = argparse.ArgumentParser(description=desc)
+    argparser.add_argument('model', choices=("dense", "sparse"), help="type of model")
     argparser.add_argument('infile', help="binary model file to read")
     argparser.add_argument('outfile', nargs="?", help="tsv file to write (if missing, <infile>.tsv)")
     args = argparser.parse_args()
 
-    model = perceptron.Perceptron()
+    model = DensePerceptron() if args.model == "dense" else SparsePerceptron()
     model.load(args.infile, util)
     model.write(args.outfile or os.path.splitext(args.infile)[0] + ".tsv")
 
