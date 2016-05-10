2c56f77525715e13a5bbb4e3f8f3c5a87736e92e
Daniel Hershcovich <danielh@cs.huji.ac.il>
2016-05-10 13:53:51 +0300

    Move more constants to config

--- scripts/model_to_tsv.py
+++ scripts/model_to_tsv.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import sys
 
+from parsing import config
 from parsing.classifiers.dense_perceptron import DensePerceptron
 from parsing.classifiers.sparse_perceptron import SparsePerceptron
 
@@ -13,12 +14,12 @@ desc = """Reads a model file in pickle format and writes as TSV
 
 def main():
     argparser = argparse.ArgumentParser(description=desc)
-    argparser.add_argument('model', choices=("dense", "sparse"), help="type of model")
+    argparser.add_argument('model', choices=(config.SPARSE_PERCEPTRON, config.DENSE_PERCEPTRON), help="type of model")
     argparser.add_argument('infile', help="binary model file to read")
     argparser.add_argument('outfile', nargs="?", help="tsv file to write (if missing, <infile>.tsv)")
     args = argparser.parse_args()
 
-    model = DensePerceptron() if args.model == "dense" else SparsePerceptron()
+    model = DensePerceptron() if args.model == config.DENSE_PERCEPTRON else SparsePerceptron()
     model.load(args.infile)
     model.write(args.outfile or os.path.splitext(args.infile)[0] + ".tsv")
 
