c4ab2313683495f80e8f6adace5bd44b22db5e76
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2016-10-12 17:16:22 +0300

    Install DyNet so Travis CI can work

---
+++ ci/install-dynet.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+export CXX="g++-4.8" CC="gcc-4.8"
+cd $HOME
+git clone https://github.com/clab/dynet
+cd dynet
+hg clone https://bitbucket.org/eigen/eigen
+wget https://raw.githubusercontent.com/bplank/bilstm-aux/master/dynet_py3_patch.diff
+git apply dynet_py3_patch.diff
+mkdir build
+cd build
+cmake .. -DEIGEN3_INCLUDE_DIR=eigen -DPYTHON=`which python`
+make
+cd python
+python setup.py install
