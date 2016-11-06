858623bf2d3cc46709296946c9b90a344015bc6b
Daniel Hershcovich <danielh@cs.huji.ac.il>
2016-11-06 14:26:56 +0200

    Use my python3 branch of DyNet rather than using a patch

--- ci/install-dynet.sh
+++ ci/install-dynet.sh
@@ -2,11 +2,9 @@
 
 export CXX="g++-4.8" CC="gcc-4.8"
 cd $HOME
-git clone https://github.com/clab/dynet
+git clone https://github.com/danielhers/dynet -b python3
 cd dynet
 hg clone https://bitbucket.org/eigen/eigen
-wget https://raw.githubusercontent.com/danielhers/bilstm-aux/master/dynet_py3_patch.diff
-git apply dynet_py3_patch.diff
 mkdir build
 cd build
 cmake .. -DEIGEN3_INCLUDE_DIR=eigen -DPYTHON=`which python`
