64e0cec34b928d72268861546c9911e60e2bcb19
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2016-11-18 08:35:31 +0200

    Import model type names directly instead of importing config

--- scripts/model_to_tsv.py
+++ scripts/model_to_tsv.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 import argparse
+import os
 import sys
 
-import os
 from linear.dense_perceptron import DensePerceptron
 from linear.sparse_perceptron import SparsePerceptron
-
-from parsing import config
+from parsing.config import SPARSE_PERCEPTRON, DENSE_PERCEPTRON
 
 desc = """Reads a model file in pickle format and writes as TSV
 """
@@ -15,12 +14,12 @@ desc = """Reads a model file in pickle format and writes as TSV
 
 def main():
     argparser = argparse.ArgumentParser(description=desc)
-    argparser.add_argument('model', choices=(config.SPARSE_PERCEPTRON, config.DENSE_PERCEPTRON), help="type of model")
+    argparser.add_argument('model', choices=(SPARSE_PERCEPTRON, DENSE_PERCEPTRON), help="type of model")
     argparser.add_argument('infile', help="binary model file to read")
     argparser.add_argument('outfile', nargs="?", help="tsv file to write (if missing, <infile>.tsv)")
     args = argparser.parse_args()
 
-    model = DensePerceptron() if args.model == config.DENSE_PERCEPTRON else SparsePerceptron()
+    model = DensePerceptron() if args.model == DENSE_PERCEPTRON else SparsePerceptron()
     model.load(args.infile)
     model.write(args.outfile or os.path.splitext(args.infile)[0] + ".tsv")
 
