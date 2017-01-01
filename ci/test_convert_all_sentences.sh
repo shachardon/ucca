ceffeeefb6e962d8446f206d19bea0a304f2db4f
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-01-01 11:03:15 +0200

    Skip failed conversions in convert_and_evaluate
    Also remove conversion to/from "txt" to save time.

--- ci/test_convert_all_sentences.sh
+++ ci/test_convert_all_sentences.sh
@@ -2,14 +2,14 @@
 
 cd $(dirname $0)
 mkdir -p converted-sentences
-for format in conll sdp export "export --tree" txt; do
-    echo Evaluating $format ...
+for format in conll sdp export "export --tree"; do
+    echo === Evaluating $format ===
     if [ $# -lt 1 -o "$format" = "$1" ]; then
         for passage in ../pickle/sentences/ucca_passage*.pickle; do
             base=$(basename $passage .pickle)
-            python3 ../scripts/convert_from_standard.py $passage -f $format -o converted-sentences -p "ucca_passage" || exit 1
-            python3 ../scripts/convert_to_standard.py converted-sentences/$base.$format -f $format -o converted-sentences -b -p "ucca_passage" || exit 1
-            python3 ../scripts/evaluate_standard.py -f converted-sentences/$base.pickle $passage | grep F1 || exit 1
+            python ../scripts/convert_from_standard.py $passage -f $format -o converted-sentences -p "ucca_passage" || exit 1
+            python ../scripts/convert_to_standard.py converted-sentences/$base.$format -f $format -o converted-sentences -b -p "ucca_passage" || exit 1
+            python ../scripts/evaluate_standard.py -f converted-sentences/$base.pickle $passage | grep F1 || exit 1
         done | tee "$format.sentences.log"
     fi
 done
