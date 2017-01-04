6e0bccd144fd0bb29cc3c2e819fcb8bc818f2310
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-01-04 16:22:13 +0200

    Generalize evaluate_standard to compare directories

--- scripts/evaluate_standard.py
+++ scripts/evaluate_standard.py
@@ -4,9 +4,7 @@ The evaluation script for UCCA layer 1.
 """
 from argparse import ArgumentParser
 
-from ucca.evaluation import evaluate
-from ucca.ioutil import file2passage
-from ucca import constructions
+from ucca import evaluation, constructions, ioutil
 
 
 if __name__ == "__main__":
@@ -19,14 +17,32 @@ if __name__ == "__main__":
                            help="outputs the traditional P,R,F instead of the scene structure evaluation")
     argparser.add_argument("-e", "--errors", action="store_true",
                            help="prints the error distribution according to its frequency")
+    argparser.add_argument("-v", "--verbose", action="store_true",
+                           help="prints the results for every single pair (always true if there is only one pair)")
     constructions.add_argument(argparser)
     args = argparser.parse_args()
 
     if not (args.units or args.fscore or args.errors):
         argparser.error("At least one of -u, -f or -e is required.")
 
-    guessed, ref = [file2passage(x) for x in (args.guessed, args.ref)]
-
-    if args.units or args.fscore or args.errors:
-        evaluate(guessed, ref, constructions=args.constructions,
-                 units=args.units, fscore=args.fscore, errors=args.errors, verbose=True)
+    guessed, ref = [ioutil.read_files_and_dirs((x,)) for x in (args.guessed, args.ref)]
+    if len(guessed) != len(ref):
+        raise ValueError("Number of passages to compare does not match: %d != %d" % (len(guessed), len(ref)))
+    if len(guessed) > 1:
+        guessed_by_id = {p.ID: p for p in guessed}
+        try:
+            guessed = [guessed_by_id[p.ID] for p in ref]
+        except KeyError as e:
+            raise ValueError("Passage IDs do not match") from e
+    results = [evaluation.evaluate(g, r,
+                                   constructions=args.constructions,
+                                   units=args.units,
+                                   fscore=args.fscore,
+                                   errors=args.errors,
+                                   verbose=args.verbose or len(guessed) == 1)
+               for g, r in zip(guessed, ref)]
+    scores = evaluation.Scores.aggregate(results)
+    print("\nAverage labeled F1 score: %.3f" % scores.average_f1())
+    if len(results) > 1:
+        print("Aggregated scores:")
+        scores.print()
