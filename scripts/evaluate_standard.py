33171c3a91872f8187ae457f22d13c32cc27b5fe
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-01-09 17:28:27 +0200

    Minor fix to output of evaluate_standard [ci skip]

--- scripts/evaluate_standard.py
+++ scripts/evaluate_standard.py
@@ -44,10 +44,9 @@ if __name__ == "__main__":
     results = []
     for g, r in zip(guessed, ref):
         if len(guessed) > 1:
-            sys.stdout.write("\rEvaluating %s:" % g.ID)
+            sys.stdout.write("\rEvaluating %s%s" % (g.ID, ":" if args.verbose else "..."))
             sys.stdout.flush()
-        if args.verbose:
-            print()
+        print()
         scores = evaluation.evaluate(g, r, constructions=args.constructions, units=args.units, fscore=args.fscore,
                                      errors=args.errors, verbose=args.verbose or len(guessed) == 1)
         if args.verbose:
@@ -57,5 +56,7 @@ if __name__ == "__main__":
     if len(results) > 1:
         if args.verbose:
             print("Aggregated scores:")
+        else:
+            print(end="\r")
         scores.print()
         print("Average labeled F1 score: %.3f" % scores.average_f1())
