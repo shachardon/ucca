117ad3e54f81e825aa32675e01b2f617187adb32
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-02-20 13:55:12 +0200

    Draw remote edges dashed, implicit nodes as text
    Also remove line width as it is ignored anyway

--- ucca/visualization.py
+++ ucca/visualization.py
@@ -6,6 +6,7 @@ import matplotlib.cbook
 import networkx as nx
 
 from ucca import layer0, layer1
+from ucca.layer1 import Linkage
 
 warnings.filterwarnings("ignore", category=matplotlib.cbook.mplDeprecation)
 warnings.filterwarnings("ignore", category=UserWarning)
@@ -14,18 +15,26 @@ warnings.filterwarnings("ignore", category=UserWarning)
 def draw(passage):
     G = nx.DiGraph()
     terminals = sorted(passage.layer(layer0.LAYER_ID).all, key=operator.attrgetter("position"))
-    G.add_nodes_from([(n.ID, {"t": n.text, "c": "white"}) for n in terminals])
-    G.add_nodes_from([n.ID for n in passage.layer(layer1.LAYER_ID).all])
-    G.add_edges_from([(n.ID, e.child.ID, {"l": e.tag}) for layer in passage.layers for n in layer.all for e in n])
+    G.add_nodes_from([(n.ID, {"label": n.text, "node_color": "white"}) for n in terminals])
+    G.add_nodes_from([(n.ID, {"label": "IMPLICIT" if n.attrib.get("implicit") else "",
+                              "node_color": "gray" if isinstance(n, Linkage) else (
+                                  "white" if n.attrib.get("implicit") else "black")})
+                      for n in passage.layer(layer1.LAYER_ID).all])
+    G.add_edges_from([(n.ID, e.child.ID, {"label": e.tag, "style": "dashed" if e.attrib.get("remote") else "solid"})
+                      for layer in passage.layers for n in layer.all for e in n])
     pos = topological_layout(passage)
-    nx.draw(G, pos, arrows=False, linewidths=0, node_color=[d.get("c", "black") for n, d in G.nodes(data=True)])
-    nx.draw_networkx_edge_labels(G, pos, edge_labels={(u, v): d["l"] for u, v, d in G.edges(data=True)}, font_size=8)
-    nx.draw_networkx_labels(G, pos, labels={n: d.get("t", "") for n, d in G.nodes(data=True)}, font_size=10)
+    nx.draw(G, pos, arrows=False, font_size=10,
+            node_color=[d["node_color"] for _, d in G.nodes(data=True)],
+            labels={n: d["label"] for n, d in G.nodes(data=True) if d["label"]},
+            style=[d["style"] for _, _, d in G.edges(data=True)])
+    nx.draw_networkx_edge_labels(G, pos, font_size=8,
+                                 edge_labels={(u, v): d["label"] for u, v, d in G.edges(data=True)})
 
 
 def topological_layout(passage):
     visited = defaultdict(set)
     pos = {}
+    implicit_offset = 1 + max((n.position for n in passage.layer(layer0.LAYER_ID).all), default=-1)
     remaining = [n for layer in passage.layers for n in layer.all if not n.parents]
     while remaining:
         node = remaining.pop()
@@ -39,6 +48,9 @@ def topological_layout(passage):
                 continue
             xs, ys = zip(*(pos[c.ID] for c in node.children))
             pos[node.ID] = (sum(xs) / len(xs), 1 + max(ys))  # done with children
-        else:  # leaf
+        elif node.layer.ID == layer0.LAYER_ID:  # terminal
             pos[node.ID] = (int(node.position), 0)
+        else:  # implicit
+            pos[node.ID] = (implicit_offset, 0)
+            implicit_offset += 1
     return pos
