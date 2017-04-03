677841816ce0578e3fbb69cbbfb0832db79a7e58
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-04-03 14:23:38 +0300

    Readable printing of attrib dicts in case of diff in --verify

--- ucca/diffutil.py
+++ ucca/diffutil.py
@@ -40,7 +40,7 @@ def diff_passages(true_passage, pred_passage):
             node_lines = []
             if not pred_node._attrib.equals(true_node._attrib):
                 node_lines.append("  Attributes mismatch: %s, %s" %
-                                  (true_node._attrib, pred_node._attrib))
+                                  (sorted(true_node._attrib.items()), sorted(pred_node._attrib.items())))
             if pred_edges:
                 node_lines.append("  Mistake edges: %s" % ", ".join(pred_edges))
             if true_edges:
