d7ca21180d00832ebe9d8ef1aaa15491427adc9a
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-08-27 17:29:25 +0300

    Update version to 1.0.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except (IOError, ImportError, RuntimeError):
 
 
 setup(name="UCCA",
-      version="1.0.5",
+      version="1.0.6",
       install_requires="spacy",
       description="Universal Conceptual Cognitive Annotation",
       long_description=long_description,
