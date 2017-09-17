f1e4ef398a21464ec94efaaff0692cf697dd752e
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-09-17 17:08:33 +0300

    Fix export --tree in conversion

--- ci/test.sh
+++ ci/test.sh
@@ -17,6 +17,6 @@ mkdir -p converted
 for FORMAT in conll sdp export "export --tree"; do
     echo === Evaluating $FORMAT ===
     if [ $# -lt 1 -o "$FORMAT" = "$1" ]; then
-        python ../scripts/convert_and_evaluate.py "$PASSAGES" -f "$FORMAT" | tee "$FORMAT.log"
+        python ../scripts/convert_and_evaluate.py "$PASSAGES" -f $FORMAT | tee "$FORMAT.log"
     fi
 done
