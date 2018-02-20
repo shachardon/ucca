c892d051c3fc1df16fee6f69a3eac24568ccad5b
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-02-20 21:57:24 +0200

    Update version to 1.0.47

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ except (IOError, ImportError, RuntimeError):
 
 
 setup(name="UCCA",
-      version="1.0.46",
+      version="1.0.47",
       install_requires=["numpy", "spacy==2.0.8", "requests", "tqdm"],
       extras_require={"visualize": ["matplotlib", "networkx"]},
       description="Universal Conceptual Cognitive Annotation",
