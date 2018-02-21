187beeeaac5da0dfcd5131e37fa8bb86adbeb72d
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-02-21 18:21:33 +0200

    Support writing tikz code for visualization

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -8,7 +8,11 @@ from ucca.ioutil import read_files_and_dirs
 if __name__ == "__main__":
     argparser = ArgumentParser(description="Visualize the given passages as graphs.")
     argparser.add_argument("passages", nargs="+", help="UCCA passages, given as xml/pickle file names")
+    argparser.add_argument("--tikz", action="store_true", help="print tikz code rather than showing plots")
     args = argparser.parse_args()
     for passage in read_files_and_dirs(args.passages):
-        visualization.draw(passage)
-        plt.show()
+        if args.tikz:
+            print(visualization.tikz(passage))
+        else:
+            visualization.draw(passage)
+            plt.show()
--- ucca/visualization.py
+++ ucca/visualization.py
@@ -54,3 +54,44 @@ def topological_layout(passage):
             pos[node.ID] = (implicit_offset, 0)
             implicit_offset += 1
     return pos
+
+
+def tikz(p, indent=None):
+    # child {node (After) [word] {After} edge from parent node[above] {\scriptsize $L$}}
+    # child {node (graduation) [circle] {}
+    # {
+    # child {node [word] {graduation} edge from parent node[left] {\scriptsize $P$}}
+    # } edge from parent node[right] {\scriptsize $H$} }
+    # child {node [word] {,} edge from parent node[below] {\scriptsize $U$}}
+    # child {node (moved) [circle] {}
+    # {
+    # child {node (John) [word] {John} edge from parent node[left] {\scriptsize $A$}}
+    # child {node [word] {moved} edge from parent node[left] {\scriptsize $P$}}
+    # child {node [circle] {}
+    # {
+    # child {node [word] {to} edge from parent node[left] {\scriptsize $R$}}
+    # child {node [word] {Paris} edge from parent node[right] {\scriptsize $C$}}
+    # } edge from parent node[right] {\scriptsize $A$} }
+    # } edge from parent node[right] {\scriptsize $H$} }
+    # ;
+    # \draw[dashed,->] (graduation) to node [auto] {\scriptsize $A$} (John);
+    if indent is None:
+        return r"""
+\begin{tikzpicture}[->,level distance=26mm,
+  level 1/.style={sibling distance=10cm},
+  level 2/.style={sibling distance=7cm},
+  level 3/.style={sibling distance=2cm},
+  every circle node/.append style={fill=black}]
+  \tikzstyle{word} = [font=\rmfamily,color=black]
+  """ + "\\" + tikz(p.layer(layer1.LAYER_ID).heads[0], indent=1) + r"""
+;
+\end{tikzpicture}"""
+    s = "node (" + p.ID.replace(".", "_") + ") "
+    if p.terminals:
+        s += "[word] {" + " ".join(t.text for t in p.terminals) + "} "
+    else:
+        s += ("\n" + indent * "  ").join(["[circle] {}", "{"] +
+                                         ["child {" + tikz(e.child, indent + 1) +
+                                          "edge from parent node[auto] {\scriptsize $" + e.tag + "$}}"
+                                          for e in p.outgoing if not e.attrib.get("remote")]) + " }"
+    return s
