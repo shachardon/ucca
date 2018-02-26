8c3c136d5dbb464283b479112830880d5fbd314e
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-02-26 19:46:41 +0200

    Update spaCy to 2.0.9

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ except (IOError, ImportError, RuntimeError):
 
 setup(name="UCCA",
       version="1.0.47",
-      install_requires=["numpy", "spacy==2.0.8", "requests", "tqdm"],
+      install_requires=["numpy", "spacy==2.0.9", "requests", "tqdm"],
       extras_require={"visualize": ["matplotlib", "networkx"]},
       description="Universal Conceptual Cognitive Annotation",
       long_description=long_description,
