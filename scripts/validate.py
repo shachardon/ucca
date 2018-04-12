ce357f37be6910ce0e53d0f86620df1fd0d87fac
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-04-12 12:33:50 +0300

    Fix validation script to detect cycles properly and print all errors

--- scripts/validate.py
+++ scripts/validate.py
@@ -1,15 +1,23 @@
 import argparse
+import sys
 
 from ucca.ioutil import get_passages_with_progress_bar
 from ucca.validation import validate
 
 
 def main(args):
-    for passage in get_passages_with_progress_bar(args.filenames, desc="Validating"):
-        validate(passage)
+    errors = {p.ID: list(validate(p)) for p in get_passages_with_progress_bar(args.filenames, desc="Validating")}
+    if any(errors.values()):
+        id_len = max(map(len, errors))
+        for passage_id, es in sorted(errors.items()):
+            for i, e in enumerate(es):
+                print("%-*s|%s" % (id_len, "" if i else passage_id, e))
+        sys.exit(1)
+    else:
+        print("No errors found.")
 
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description="Validate UCCA passages")
-    argparser.add_argument("filenames", help="files or directories to validate")
+    argparser.add_argument("filenames", nargs="+", help="files or directories to validate")
     main(argparser.parse_args())
--- ucca/validation.py
+++ ucca/validation.py
@@ -2,15 +2,29 @@ from ucca import layer0, layer1
 
 
 def validate(passage):
+    stack = [list(passage.layer(layer1.LAYER_ID).heads)]
+    for node in stack[-1]:
+        if node.ID != "1.1" and node.tag != layer1.NodeTags.Linkage:
+            yield "Extra root (%s)" % node.ID
     visited = set()
-    nodes = list(passage.layer(layer1.LAYER_ID).all)
-    while nodes:
-        node = nodes.pop(0)
-        visited |= set(node)
-        for edge in node:
-            assert edge not in visited, "Detected cycle: %s" % edge
-            assert (edge.tag == layer1.EdgeTags.Punctuation) == (edge.child.tag == layer1.NodeTags.Punctuation), \
-                "%s edge to %s node" % (edge.tag, edge.child.tag)
-            assert node.tag == layer1.NodeTags.Punctuation == edge.child.tag == layer0.NodeTags.Punct, \
-                "%s parent for %s node" % (node.tag, edge.child.tag)
-            nodes.append(edge.child)
+    path = []
+    path_set = set(path)
+    while stack:
+        for node in stack[-1]:
+            if node in path_set:
+                yield "Detected cycle (%s)" % "->".join(n.ID for n in path)
+            elif node not in visited:
+                visited.add(node)
+                path.append(node)
+                path_set.add(node)
+                stack.append(node.children)
+                for edge in node:
+                    if (edge.tag == layer1.EdgeTags.Punctuation) != (edge.child.tag == layer1.NodeTags.Punctuation):
+                        yield "%s edge (%s) with %s child" % (edge.tag, edge, edge.child.tag)
+                    if (node.tag == layer1.NodeTags.Punctuation) != (edge.child.tag == layer0.NodeTags.Punct):
+                        yield "%s node (%s) with %s child (%s)" % (node.tag, node.ID, edge.child.tag, edge.child.ID)
+                break
+        else:
+            if path:
+                path_set.remove(path.pop())
+            stack.pop()
