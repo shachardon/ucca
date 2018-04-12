1418ba613c21e54eb13672dce9b2230880c05635
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-04-12 15:13:57 +0300

    Detect multiple incoming non-remote edges in validation

--- ucca/validation.py
+++ ucca/validation.py
@@ -1,12 +1,15 @@
 from ucca import layer0, layer1
 
 
+LINKAGE = (layer1.EdgeTags.LinkArgument, layer1.EdgeTags.LinkRelation)
+
+
 def validate(passage):
     for node in passage.layer(layer0.LAYER_ID).all:
         if not node.incoming:
             yield "Orphan terminal (%s) '%s'" % (node.ID, node)
         elif len(node.incoming) > 1:
-            yield "Reentrant terminal (%s) '%s'" % (", ".join(map(str, node.incoming)), node)
+            yield "Reentrant terminal (%s) '%s'" % (join(node.incoming), node)
     stack = [list(passage.layer(layer1.LAYER_ID).heads)]
     for node in stack[-1]:
         if node.ID != "1.1" and node.tag != layer1.NodeTags.Linkage:
@@ -23,6 +26,9 @@ def validate(passage):
                 path.append(node)
                 path_set.add(node)
                 stack.append(node.children)
+                incoming = [e for e in node.incoming if not e.attrib.get("remote") and e.tag not in LINKAGE]
+                if len(incoming) > 1:
+                    yield "Multiple incoming non-remote (%s)" % join(incoming)
                 for edge in node:
                     if (edge.tag == layer1.EdgeTags.Punctuation) != (edge.child.tag == layer1.NodeTags.Punctuation):
                         yield "%s edge (%s) with %s child" % (edge.tag, edge, edge.child.tag)
@@ -33,3 +39,7 @@ def validate(passage):
             if path:
                 path_set.remove(path.pop())
             stack.pop()
+
+
+def join(incoming):
+    return ", ".join(map(str, incoming))
