2de343c6029540a28ceee42500ad96fe9549e1e4
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-05-05 13:22:22 +0300

    Fix output directory handling in site_to_package

--- scripts/site_to_standard.py
+++ scripts/site_to_standard.py
@@ -7,6 +7,7 @@ import sqlite3
 from xml.etree.ElementTree import ElementTree, tostring, fromstring
 
 import ucca.convert
+from ioutil import write_passage
 from ucca.textutil import indent_xml
 
 desc = """Parses an XML in UCCA site format.
@@ -24,51 +25,25 @@ or using filenames of a site-formatted XML file.
 def site2passage(filename):
     """Opens a file and returns its parsed Passage object"""
     with open(filename, encoding="utf-8") as f:
-        etree = ElementTree().parse(f)
-    return ucca.convert.from_site(etree)
+        return ucca.convert.from_site(ElementTree().parse(f))
 
 
 def db2passage(handle, pid, user):
     """Gets the annotation of user to pid from the DB handle - returns a passage"""
     handle.execute("SELECT id FROM users WHERE username=?", (user,))
     uid = handle.fetchone()[0]
-    handle.execute("SELECT xml FROM xmls WHERE paid=? AND uid=? " +
-                   "ORDER BY ts DESC", (pid, uid))
-    raw_xml = handle.fetchone()[0]
-    return ucca.convert.from_site(fromstring(raw_xml))
-
-
-def outfile(source, target, suffix):
-    return os.path.join(target, os.path.splitext(source)[0] + suffix) if os.path.isdir(target) else target
+    handle.execute("SELECT xml FROM xmls WHERE paid=? AND uid=? ORDER BY ts DESC", (pid, uid))
+    return ucca.convert.from_site(fromstring(handle.fetchone()[0]))
 
 
 def main(args):
-    if args.filenames:
-        passages = ((filename, site2passage(filename)) for filename in args.filenames)
-    else:
-        conn = sqlite3.connect(args.db)
-        c = conn.cursor()
-        passages = ((pid, db2passage(c, pid, args.user)) for pid in args.pids)
-
-    for filename, passage in passages:
-        if args.binary:
-            with open(outfile(filename, args.binary, ".pickle"), "wb") as binf:
-                pickle.dump(passage, binf)
-        else:
-            root = ucca.convert.to_standard(passage)
-            output = indent_xml(tostring(root).decode())
-            if args.outfile:
-                with open(outfile(filename, args.outfile, ".xml"), "w", encoding="utf-8") as outf:
-                    outf.write(output)
-            else:
-                print(output)
+    os.makedirs(args.out_dir, exist_ok=True)
+    for filename, passage in ((filename, site2passage(filename)) for filename in args.filenames) if args.filenames \
+            else ((pid, db2passage(sqlite3.connect(args.db).cursor(), pid, args.user)) for pid in args.pids):
+        write_passage(passage, outdir=args.out_dir, binary=args.binary)
 
 
 def check_illegal_combinations(args):
-    if args.db and args.filenames:
-        argparser.error("Only one source, XML or DB file, can be used")
-    if (not args.db) and (not args.filenames):
-        argparser.error("Must specify one source, XML or DB file")
     if args.db and not (args.pids and args.user):
         argparser.error("Must specify a username and a passage ID when " +
                         "using DB file option")
@@ -79,10 +54,11 @@ def check_illegal_combinations(args):
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description=desc)
-    argparser.add_argument("filenames", nargs="*", help="XML file name to convert")
-    argparser.add_argument("-o", "--outfile", help="output file for standard XML")
+    group = argparser.add_mutually_exclusive_group(required=True)
+    group.add_argument("filenames", nargs="*", help="XML file name to convert")
+    group.add_argument("-d", "--db", help="DB file to get input from")
+    argparser.add_argument("-o", "--out-dir", default=".", help="output directory for standard XML")
     argparser.add_argument("-b", "--binary", help="output file for binary pickle")
-    argparser.add_argument("-d", "--db", help="DB file to get input from")
     argparser.add_argument("-p", "--pids", nargs="*", type=int, help="PassageIDs to query DB")
     argparser.add_argument("-u", "--user", help="Username to DB query")
     main(check_illegal_combinations(argparser.parse_args()))
--- ucca/ioutil.py
+++ ucca/ioutil.py
@@ -132,7 +132,7 @@ def read_files_and_dirs(files_and_dirs, sentences=False, paragraphs=False, conve
 
 def write_passage(passage, output_format=None, binary=False, outdir=".", prefix="", converter=None, verbose=True):
     suffix = output_format if output_format and output_format != "ucca" else ("pickle" if binary else "xml")
-    outfile = outdir + os.path.sep + prefix + passage.ID + "." + suffix
+    outfile = os.path.join(outdir, prefix + passage.ID + "." + suffix)
     if verbose:
         with tqdm.external_write_mode():
             print("Writing passage '%s'..." % outfile)
