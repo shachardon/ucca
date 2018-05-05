d5808af3a115030dc83f9e3e4b0f76a77cefdbcf
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-05-05 12:18:11 +0300

    Update version to 1.0.67

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ except (IOError, ImportError, RuntimeError):
 
 
 setup(name="UCCA",
-      version="1.0.66",
+      version="1.0.67",
       install_requires=["numpy", "spacy==2.0.11", "requests", "tqdm"],
       extras_require={"visualize": ["matplotlib", "networkx"],
                       "distances": ["distances", "zss", "munkres"]},
