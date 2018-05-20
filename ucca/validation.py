98fae1af615c75ab0ed5d17299c8df194ca23ea3
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-05-20 10:19:04 +0300

    More validations

--- ucca/validation.py
+++ ucca/validation.py
@@ -34,53 +34,72 @@ def validate(passage):
                 path.append(node)
                 path_set.add(node)
                 stack.append(node.children)
+                incoming = tag_to_edge(node.incoming)
+                outgoing = tag_to_edge(node)
+                incoming_tags = set(incoming)
+                outgoing_tags = set(outgoing)
                 if node.tag == L1Tags.Linkage:
                     if node.incoming:
-                        yield "Non-root %s node (%s)" % (node.tag, join(node.incoming))
-                    non_linkage = [e for e in node if e.tag not in LINKAGE]
-                    if non_linkage:
-                        yield "%s node with non-linkage children (%s)" % (node.tag, join(non_linkage))
-                    if not any(e.tag == ETags.LinkRelation for e in node):
+                        yield "Non-root %s node (%s)" % (node.tag, node)
+                    s = outgoing_tags.difference(LINKAGE)
+                    if s:
+                        yield "%s node (%s) with %s children" % (node.tag, node, join(s))
+                    if ETags.LinkRelation not in outgoing:
                         yield "%s node without %s child" % (node.tag, ETags.LinkRelation)
                 elif node.tag == L1Tags.Foundational:
                     if node.participants and not node.is_scene():
                         yield "Node (%s) with participants but without main relation" % node.ID
                     if node.process and node.state:
                         yield "Node (%s) with both process (%s) and state (%s)" % (node.ID, node.process, node.state)
-                    scenes = node.parallel_scenes
-                    for edge in node:
-                        if scenes and edge.tag not in (ETags.ParallelScene, ETags.Punctuation, ETags.Linker,
-                                                       ETags.Ground, ETags.Relator, ETags.Function):
-                            yield "Node (%s) with parallel scenes has %s edge (%s)" % (node.ID, edge.tag, edge)
-                        if edge.tag in LINKAGE:
-                            yield "Non-linkage node with %s edge (%s)" % (edge.tag, edge)
-                incoming = [e for e in node.incoming if not e.attrib.get("remote") and e.tag not in LINKAGE]
-                if len(incoming) > 1:
-                    yield "Multiple incoming non-remote (%s)" % join(incoming)
-                is_function = any(e.tag == ETags.Function for e in node.incoming)
+                    if node.parallel_scenes:
+                        s = outgoing_tags.difference((ETags.ParallelScene, ETags.Punctuation, ETags.Linker,
+                                                           ETags.Ground, ETags.Relator, ETags.Function))
+                        if s:
+                            yield "Node (%s) with parallel scenes has %s edge" % (node.ID, join(s))
+                    s = outgoing_tags.intersection(LINKAGE)
+                    if s:
+                        yield "Non-linkage node (%s) with %s edges" % (node, join(s))
+                primary_incoming = [e for e in node.incoming if not e.attrib.get("remote") and e.tag not in LINKAGE]
+                if len(primary_incoming) > 1:
+                    yield "Multiple incoming non-remote (%s)" % join(primary_incoming)
                 for edge in node:
                     if (edge.tag == ETags.Punctuation) != (edge.child.tag == L1Tags.Punctuation):
                         yield "%s edge (%s) with %s child" % (edge.tag, edge, edge.child.tag)
                     if (node.tag == L1Tags.Punctuation) != (edge.child.tag == L0Tags.Punct):
                         yield "%s node (%s) with %s child (%s)" % (node.tag, node.ID, edge.child.tag, edge.child.ID)
-                    if is_function and edge.tag not in (ETags.Terminal, ETags.Punctuation):
-                        yield "Function node with outgoing %s edge (%s)" % (edge.tag, edge)
                 if node.attrib.get("implicit") and node.outgoing:
                     yield "Implicit node (%s) with outgoing edges (%s)" % (node.ID, join(node))
-                for unique_tag in (ETags.Function, ETags.Ground, ETags.ParallelScene, ETags.Linker, ETags.LinkRelation,
+                for tag in (ETags.Function, ETags.Ground, ETags.ParallelScene, ETags.Linker, ETags.LinkRelation,
                                    ETags.Connector, ETags.Punctuation, ETags.Terminal):
-                    unique_incoming = [e for e in node.incoming if e.tag == unique_tag]
-                    if len(unique_incoming) > 1:
-                        yield "Multiple incoming %s edges (%s)" % (unique_tag, join(unique_incoming))
-                for unique_tag in (ETags.LinkRelation, ETags.Process, ETags.State):
-                    unique_outgoing = [e for e in node if e.tag == unique_tag]
-                    if len(unique_outgoing) > 1:
-                        yield "Multiple outgoing %s edges (%s)" % (unique_tag, join(unique_outgoing))
+                    s = incoming.get(tag, ())
+                    if len(s) > 1:
+                        yield "Multiple incoming %s edges (%s)" % (tag, join(s))
+                for tag in (ETags.LinkRelation, ETags.Process, ETags.State):
+                    s = outgoing.get(tag, ())
+                    if len(s) > 1:
+                        yield "Multiple outgoing %s edges (%s)" % (tag, join(s))
+                if ETags.Function in incoming:
+                    s = outgoing_tags.difference(ETags.Terminal, ETags.Punctuation)
+                    if s:
+                        yield "%s node (%s) with outgoing %s edge" % (ETags.Function, node.ID, join(s))
+                if ETags.Linker in incoming_tags:
+                    s = incoming_tags.difference((ETags.Linker, ETags.LinkRelation))
+                    if s:
+                        yield "%s node (%s) with incoming %s edge" % (ETags.Linker, node.ID, join(s))
+                    if ETags.LinkRelation not in incoming_tags:
+                        yield "%s node (%s) with no incoming %s" % (ETags.Linker, node.ID, ETags.LinkRelation)
                 break
         else:
             if path:
                 path_set.remove(path.pop())
             stack.pop()
+
+
+def tag_to_edge(edges):
+    d = {}
+    for edge in edges:
+        d.setdefault(edge.tag, []).append(edge)
+    return d
 
 
 def join(items):
