2cd98127881282332370a518db1354b62c13f06f
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-05-31 16:57:53 +0300

    Option to normalize passages before validation

--- scripts/validate.py
+++ scripts/validate.py
@@ -3,12 +3,19 @@ import sys
 import argparse
 
 from ucca.ioutil import get_passages_with_progress_bar
+from ucca.normalization import normalize
 from ucca.validation import validate
 
 
+def validate_passage(passage, normalization=False, extra=False):
+    if normalization:
+        normalize(passage, extra=extra)
+    return list(validate(passage))
+
+
 def main(args):
-    errors = ((p.ID, list(validate(p))) for p in get_passages_with_progress_bar(args.filenames, desc="Validating",
-                                                                                converters={}))
+    errors = ((p.ID, validate_passage(p, args.normalize, args.extra))
+              for p in get_passages_with_progress_bar(args.filenames, desc="Validating", converters={}))
     errors = {k: v for k, v in errors if v}
     if errors:
         id_len = max(map(len, errors))
@@ -20,7 +27,15 @@ def main(args):
         print("No errors found.")
 
 
+def check_args(parser, args):
+    if args.extra and not args.normalize:
+        parser.error("Cannot specify --extra without --normalize")
+    return args
+
+
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description="Validate UCCA passages")
     argparser.add_argument("filenames", nargs="+", help="files or directories to validate")
-    main(argparser.parse_args())
+    argparser.add_argument("-n", "--normalize", action="store_true", help="normalize before validation")
+    argparser.add_argument("-e", "--extra", action="store_true", help="extra normalization rules")
+    main(check_args(argparser, argparser.parse_args()))
