15c7b589237534cae73676fddf9f4031f9b648bc
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-06-10 16:57:10 +0300

    Change evaluate_db to use ArgumentParser instead of OptionParser

--- scripts/evaluate_db.py
+++ scripts/evaluate_db.py
@@ -3,7 +3,7 @@
 The evaluation software for UCCA layer 1.
 """
 
-from optparse import OptionParser
+from argparse import ArgumentParser
 
 from scripts import ucca_db
 from ucca import convert
@@ -14,65 +14,37 @@ from ucca.evaluation import evaluate
 # Returns the command line parser.
 ##############################################################################
 def cmd_line_parser():
-    usage = "usage: %prog [options]\n"
-    opt_parser = OptionParser(usage=usage)
-    opt_parser.add_option("--db", "-d", dest="db_filename",
-                          action="store", type="string",
-                          help="the db file name")
-    opt_parser.add_option("--host", "--hst", dest="host",
-                          action="store", type="string",
-                          help="the host name")
-    opt_parser.add_option("--pid", "-p", dest="pid", action="store",
-                          type="int", help="the passage ID")
-    opt_parser.add_option("--from_xids", "-x", dest="from_xids",
-                          action="store_true", help="interpret the ref \
-                          and the guessed parameters as Xids in the db")
-    opt_parser.add_option("--guessed", "-g", dest="guessed", action="store",
-                          type="string", help="if a db is defined - \
-                          the username for the guessed annotation; \
-                          else - the xml file name for the guessed annotation")
-    opt_parser.add_option("--ref", "-r", dest="ref", action="store",
-                          type="string", help="if a db is defined - \
-                          the username for the reference annotation; else - \
-                          the xml file name for the reference annotation")
-    opt_parser.add_option("--units", "-u", dest="units", action="store_true",
-                          help="the units the annotations have in common, \
-                          and those each has separately")
-    opt_parser.add_option("--fscore", "-f", dest="fscore", action="store_true",
-                          help="outputs the traditional P,R,F \
-                          instead of the scene structure evaluation")
-    opt_parser.add_option("--debug", dest="debug", action="store_true",
-                          help="run in debug mode")
-    opt_parser.add_option("--errors", "-e", dest="errors", action="store_true",
-                          help="prints the error distribution\
-                          according to its frequency")
-    return opt_parser
+    return argparser
 
 
-def main():
-    opt_parser = cmd_line_parser()
-    (options, args) = opt_parser.parse_args()
-    if len(args) > 0:
-        opt_parser.error("all arguments must be flagged")
-
-    if (options.guessed is None) or (options.ref is None) or (options.db_filename is None):
-        opt_parser.error("missing arguments. type --help for help.")
-    if options.pid is not None and options.from_xids is not None:
-        opt_parser.error("inconsistent parameters. \
-        you can't have both a pid and from_xids paramters.")
-
-    keys = [options.guessed, options.ref]
-    if options.from_xids:
-        xmls = ucca_db.get_by_xids(options.db_filename, options.host, keys)
-    else:
-        xmls = ucca_db.get_xml_trees(options.db_filename, options.host,
-                                     options.pid, keys)
-
+def main(args):
+    keys = [args.guessed, args.ref]
+    xmls = ucca_db.get_by_xids(args.db_filename, args.host, keys) if args.from_xids else \
+        ucca_db.get_xml_trees(args.db_filename, args.host, args.pid, keys)
     guessed, ref = [convert.from_site(x) for x in xmls]
-    if options.units or options.fscore or options.errors:
-        evaluate(guessed, ref,
-                 units=options.units, fscore=options.fscore, errors=options.errors, verbose=True)
+    if args.units or args.fscore or args.errors:
+        evaluate(guessed, ref, units=args.units, fscore=args.fscore, errors=args.errors, verbose=True)
 
 
 if __name__ == '__main__':
-    main()
+    argparser = ArgumentParser(description="Evaluate passages on UCCA DB")
+    argparser.add_argument("--db", "-d", required=True, dest="db_filename", help="the db file name")
+    argparser.add_argument("--host", "--hst", help="the host name")
+    group = argparser.add_mutually_exclusive_group()
+    group.add_argument("-p", "--pid", type=int, help="the passage ID")
+    group.add_argument("-x", "--from_xids", action="store_true",
+                       help="interpret the ref and the guessed parameters as Xids in the db")
+    argparser.add_argument("--guessed", "-g", required=True,
+                           help="if a db is defined - the username for the guessed annotation; "
+                                "else - the xml file name for the guessed annotation")
+    argparser.add_argument("-r", "--ref", required=True,
+                           help="if a db is defined - the username for the reference annotation; "
+                                "else - the xml file name for the reference annotation")
+    argparser.add_argument("-u", "--units", action="store_true",
+                           help="the units the annotations have in common, and those each has separately")
+    argparser.add_argument("-f", "--fscore", action="store_true",
+                           help="outputs the traditional P,R,F instead of the scene structure evaluation")
+    argparser.add_argument("--debug", action="store_true", help="run in debug mode")
+    argparser.add_argument("-e", "--errors", action="store_true",
+                           help="prints the error distribution according to its frequency")
+    main(argparser.parse_args())
