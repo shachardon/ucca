68f0dc522b5c51acb30a584c54a49c907daac570
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-06-10 14:54:34 +0300

    Test constructions with and without syntax

--- ucca/tests/test_constructions.py
+++ ucca/tests/test_constructions.py
@@ -1,15 +1,31 @@
 import pytest
 
-from ucca.constructions import extract_edges, CONSTRUCTION_BY_NAME, EDGE_TYPES_NAME
+from ucca import textutil
+from ucca.constructions import extract_edges, CONSTRUCTION_BY_NAME, EDGE_TYPES_NAME, DEFAULT
 from .conftest import PASSAGES
 
 """Tests the constructions module functions and classes."""
 
 
-@pytest.mark.parametrize("create", PASSAGES)
-def test_evaluate_self(create):
-    p = create()
-    d = extract_edges(p)
-    for construction in CONSTRUCTION_BY_NAME:
+def assert_spacy_not_loaded(*args, **kwargs):
+    del args, kwargs
+    assert False, "Should not load spaCy when passage is pre-annotated"
+
+
+def extract_and_check(p, constructions=None):
+    d = extract_edges(p, constructions=constructions)
+    for construction in constructions or CONSTRUCTION_BY_NAME:
         if construction != EDGE_TYPES_NAME:
             assert construction in d
+
+
+@pytest.mark.parametrize("create", PASSAGES)
+def test_extract_all(create):
+    extract_and_check(create())
+
+
+@pytest.mark.parametrize("create", PASSAGES)
+@pytest.mark.parametrize("constructions", (DEFAULT, [EDGE_TYPES_NAME]))
+def test_extract(create, constructions, monkeypatch):
+    monkeypatch.setattr(textutil, "get_nlp", assert_spacy_not_loaded)
+    extract_and_check(create(), constructions=constructions)
