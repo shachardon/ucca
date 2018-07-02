aba3f1b09f36666c2578fd8cf24ca4697eda1712
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-07-02 11:46:48 +0300

    Fix 1.0->1.2 conversion script to annotate with as_array=False

--- scripts/convert_1_0_to_1_2.py
+++ scripts/convert_1_0_to_1_2.py
@@ -1,3 +1,4 @@
+import os
 from argparse import ArgumentParser
 
 from ucca import layer0, layer1
@@ -9,7 +10,7 @@ desc = """Convert the English Wiki corpus from version 1.0 to 1.2"""
 
 
 def get_annotation(terminal, attr):
-    return attr(terminal.tok[attr.value])
+    return terminal.extra[attr.key]
 
 
 def is_main_relation(node):
@@ -23,7 +24,7 @@ def move_node(node, new_parent, tag=None):
 
 
 def extract_aux(passage):
-    for terminal in passage.layer(layer0.LAYER_ID):
+    for terminal in passage.layer(layer0.LAYER_ID).all:
         if get_annotation(terminal, Attr.LEMMA) in {"have", "be", "will", "to"}:
             parent = fparent(terminal)
             grandparent = fparent(parent)
@@ -36,7 +37,7 @@ def extract_aux(passage):
 
 
 def extract_modal(passage):
-    for terminal in passage.layer(layer0.LAYER_ID):
+    for terminal in passage.layer(layer0.LAYER_ID).all:
         if get_annotation(terminal, Attr.LEMMA) in {"can"}:
             parent = fparent(terminal)
             grandparent = fparent(parent)
@@ -56,15 +57,16 @@ def convert_passage(passage, report_file):
 
 def main(args):
     with open(args.outfile, "w", encoding="utf-8") as f:
-        for passage in annotate_all(get_passages_with_progress_bar(args.passages), as_array=True):
+        for passage in annotate_all(get_passages_with_progress_bar(args.passages), verbose=args.verbose):
             convert_passage(passage, report_file=f)
             write_passage(passage, outdir=args.outdir, verbose=args.verbose)
+    print("Wrote '%s'" % args.outfile)
 
 
 if __name__ == "__main__":
     argparser = ArgumentParser(description=desc)
     argparser.add_argument("passages", nargs="+", help="the corpus, given as xml/pickle file names")
     argparser.add_argument("-o", "--outdir", default=".", help="output directory")
-    argparser.add_argument("-O", "--outfile", default="applied_rules.txt", help="output filename for report")
+    argparser.add_argument("-O", "--outfile", default=os.path.splitext(argparser.prog)[0] + ".log", help="log file")
     argparser.add_argument("-v", "--verbose", action="store_true", help="print tagged text for each passage")
     main(argparser.parse_args())
